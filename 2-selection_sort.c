9376c84fc46f7527eef10476e23489ba9513a799
Abdelrahman <99036432+AbdelrahmanAmrSobh@users.noreply.github.com>
2024-01-23 23:24:15 +0200

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,26 +9,26 @@
 
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_i;
-    int min_n, tmp;
+	size_t i, j, min_i;
+	int min_n, tmp;
 
-    for (i = 0; i < size; i++)
-    {
-        min_n = array[i];
-        min_i = i;
-        for (j = i; j < size; j++)
-        {
-            if (min_n > array[j])
-            {
-                min_n = array[j];
-                min_i = j;
-            }
-        }
-        if (i == min_i)
-            continue;
-        tmp = array[i];
-        array[i] = array[min_i];
-        array[min_i] = tmp;
-        print_array(array, size);
-    }
+	for (i = 0; i < size; i++)
+	{
+		min_n = array[i];
+		min_i = i;
+		for (j = i; j < size; j++)
+		{
+			if (min_n > array[j])
+			{
+				min_n = array[j];
+				min_i = j;
+			}
+		}
+		if (i == min_i)
+			continue;
+		tmp = array[i];
+		array[i] = array[min_i];
+		array[min_i] = tmp;
+		print_array(array, size);
+	}
 }
