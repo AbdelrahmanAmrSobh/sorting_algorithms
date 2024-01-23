e6cb238b1a7d84fbc78015d491a83b2213e4d1c3
Abdelrahman <99036432+AbdelrahmanAmrSobh@users.noreply.github.com>
2024-01-23 23:02:21 +0200

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,33 +8,33 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *p1 = *list, *p2 = *list;
+	listint_t *p1 = *list, *p2 = *list;
 
-    while (list && p1)
-    {
-        p1 = p1->next;
-        if (p1 && p1->prev)
-        {
-            if (p1->prev->n > p1->n)
-            {
-                p2 = p1->next;
-                while (p1->prev && p1->prev->n > p1->n)
-                {
-                    if (p1->next)
-                        p1->next->prev = p1->prev;
-                    if (p1->prev->prev)
-                        p1->prev->prev->next = p1;
-                    p1->prev->next = p1->next;
-                    p1->next = p1->prev;
-                    p1->prev = p1->prev->prev;
-                    p1->next->prev = p1;
-                    while ((*list)->prev)
-                        *list = (*list)->prev;
-                    print_list(*list);
-                }
-                if (p2)
-                    p1 = p2->prev;
-            }
-        }
-    }
+	while (list && p1)
+	{
+		p1 = p1->next;
+		if (p1 && p1->prev)
+		{
+			if (p1->prev->n > p1->n)
+			{
+				p2 = p1->next;
+				while (p1->prev && p1->prev->n > p1->n)
+				{
+					if (p1->next)
+						p1->next->prev = p1->prev;
+					if (p1->prev->prev)
+						p1->prev->prev->next = p1;
+					p1->prev->next = p1->next;
+					p1->next = p1->prev;
+					p1->prev = p1->prev->prev;
+					p1->next->prev = p1;
+					while ((*list)->prev)
+						*list = (*list)->prev;
+					print_list(*list);
+				}
+			if (p2)
+				p1 = p2->prev;
+			}
+		}
+	}
 }
