3824c6f1f853ab0b9bcbe67790d9702798514647
git <mahmoud.ahmed.ramzy@gmail.com>
2024-01-24 11:17:12 -0800

    p

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,24 +1,30 @@
 #include "sort.h"
+/**
+ * shell_sort - sorting algorithm
+ * @array: the array we want to sort
+ * @size: the array legnth
+ */
 
 void shell_sort(int *array, size_t size)
 {
 	size_t h = 1, i = 0, temp, j = 0;
-	while(h < size)
+
+	while (h < size)
 		h = 3 * h + 1;
 	h = (h - 1) / 3;
-	
-	while(h)
+
+	while (h)
 	{
 		i = 0;
-		while((i + h )< size)
+		while ((i + h) < size)
 		{
 			while (array[i] > array[i + h])
 			{
 				temp = array[i];
-				array[i] = array[i + h];  
-				array[i + h] = temp; 
+				array[i] = array[i + h];
+				array[i + h] = temp;
 				j = i;
-				while(i >= h && array[i] < array[i - h])
+				while (i >= h && array[i] < array[i - h])
 				{
 					temp = array[i];
 					array[i] = array[i - h];
@@ -32,5 +38,4 @@ void shell_sort(int *array, size_t size)
 		h = (h - 1) / 3;
 		print_array(array, size);
 	}
-
 }
