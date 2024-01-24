1aeb01ccf739d3da2165172fd564648eff4bf923
mahmoudramzy1 <97908178+mahmoudramzy1@users.noreply.github.com>
2024-01-24 21:12:14 +0200

    Update 100-shell_sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -2,37 +2,35 @@
 
 void shell_sort(int *array, size_t size)
 {
-  size_t h = 1, i = 0, temp, j = 0;
-  while(h < size)
-    h = 3 * h + 1;
-  h = (h - 1) / 3;
+	size_t h = 1, i = 0, temp, j = 0;
+	while(h < size)
+		h = 3 * h + 1;
+	h = (h - 1) / 3;
+	
+	while(h)
+	{
+		i = 0;
+		while((i + h )< size)
+		{
+			while (array[i] > array[i + h])
+			{
+				temp = array[i];
+				array[i] = array[i + h];  
+				array[i + h] = temp; 
+				j = i;
+				while(i >= h && array[i] < array[i - h])
+				{
+					temp = array[i];
+					array[i] = array[i - h];
+					array[i - h] = temp;
+					i = i - h;
+				}
+				i = j;
+			}
+			i++;
+		}
+		h = (h - 1) / 3;
+		print_array(array, size);
+	}
 
-  while(h)
-  {
-    i = 0;
-    while((i + h )< size)
-    {
-      while (array[i] > array[i + h])
-      {
-        temp = array[i];
-        array[i] = array[i + h];  
-        array[i + h] = temp; 
-        j = i;
-        while(i >= h && array[i] < array[i - h])
-        {
-          temp = array[i];
-          array[i] = array[i - h];
-          array[i - h] = temp;
-          i = i - h;
-        }
-        i = j;
-      }
-      
-      i++;
-    }
-    
-    h = (h - 1) / 3;
-    print_array(array, size);
-  }
-  
 }
